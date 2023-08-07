0151037619818a5cdab802841b617aab33a9900d
Nader Zaman <naderzaman50@gmail.com>
2023-08-07 16:40:02 +0500

    while loop

---
+++ 20.js
@@ -0,0 +1,41 @@
+// While loop
+
+// print numbers from 0 to 9
+// console.log(0);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// dry dont repeat yourself
+// let i=0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+let i=0;//(//1//2//3 true)(4 false so loop will stop)
+while(i<=9){
+    console.log(i);
+     i++;
+}
+console.log(`current value of i is ${i}`);
