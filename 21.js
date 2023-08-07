c2160cb03655b3cc7a3ce36b5845065c2b0a8358
Nader Zaman <naderzaman50@gmail.com>
2023-08-07 20:25:00 +0500

    while loop example

---
+++ 21.js
@@ -0,0 +1,28 @@
+// while loop example
+
+let num = 10;
+// let total = 0;
+// total = total+1; // 0+1
+// total = total+2; // 1+2
+// total = total+3; // 3+3
+// total = total+4; // 6+4
+// total = total+5; // 10+5
+// total = total+6; // 15+6
+// total = total+7; //21+7
+// total = total+8; // 28+8
+// total = total+9; // 36+9
+// total = total+10; // 45+10 = 55
+// console.log(total);
+
+// let total = 0;
+// let i =0;
+// while(i<=100){
+//     total = total+i;
+//     i++
+// }
+// console.log(total);
+
+
+
+let total = (num*(num+1))/2;
+console.log(total);
