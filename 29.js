bceea082f51f4dafaf49e77afc853fcd270c9647
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 22:37:08 +0500

    Update 29.js

--- 29.js
+++ 29.js
@@ -24,9 +24,10 @@
 
 // Array clone by using Spread operator
 // let array1 = ["item1","item2"];
-// let array2 = [...array1];
+// let array3 = ["item3","item4"]
+// let array2 = [...array1,...array3];
 // array1.push ("item3")
 // console.log(array1===array2);
 // console.log(array1);
 // console.log(array2);
-// how to concatenate 2 arrays
+// // how to concatenate 2 arrays
