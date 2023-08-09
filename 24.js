8fbc7589d9e62d230fcf337a4aefaede6a4c3146
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 18:38:11 +0500

    Continue and break
    Continue and break

---
+++ 24.js
@@ -0,0 +1,20 @@
+// break keyword 
+// for(let i = 1;
+//     i<=10;
+//     i++) {
+//         if(i===4){
+//             break;
+//         }
+//         console.log(i);
+//     }
+//     console.log("Hello");
+// continue keyword
+for(let i = 1;
+    i<=10;
+    i++) {
+        if(i===4){
+            continue;
+        }
+        console.log(i);
+    }
+    console.log("Hello");
