6c8fee66de608485c4ee64d08887e5ab3b5eab2e
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 18:55:12 +0500

    do while loop
    do while loop

--- 25.js
+++ 25.js
@@ -1,1 +1,10 @@
 // do while loop
+// condition will check after 1st iteration.
+let i=10;
+do{
+    console.log(i);// 10 will be printed on console due to do 
+     i++;
+    }
+
+while(i<=9);
+     
