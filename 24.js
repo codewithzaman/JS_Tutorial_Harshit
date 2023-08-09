dbbfc0ce7e4ff7462883a2b24108262a90c592e6
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 18:46:30 +0500

    updated continue and break

--- 24.js
+++ 24.js
@@ -1,4 +1,5 @@
 // break keyword 
+// Break statement is used to terminate a loop , it is used in loop and switch case.control is transfred to the outside of loop
 // for(let i = 1;
 //     i<=10;
 //     i++) {
@@ -9,12 +10,13 @@
 //     }
 //     console.log("Hello");
 // continue keyword
-for(let i = 1;
-    i<=10;
-    i++) {
-        if(i===4){
-            continue;
-        }
-        console.log(i);
-    }
-    console.log("Hello");
+// continue statement is used to continue the next iteration in the loop.it is only used in loop, control remains in the loop.
+// for(let i = 1;
+//     i<=10;
+//     i++) {
+//         if(i===4){
+//             continue;
+//         }
+//         console.log(i);
+//     }
+//     console.log("Hello");
