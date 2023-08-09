cc14e779c6feb6f98764e06ec951818bf6cfc997
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 22:13:48 +0500

    update data types

--- 28.js
+++ 28.js
@@ -12,10 +12,11 @@
 
 // reference types
 // arrays objects and functions
-let array1 = ["item1","item2"];
-let array2 = array1;
-console.log("array1", array1);
-console.log("array2", array2);
-array1.push("item3");
-console.log("array1",array1);
-console.log("array2",array2);
+// let array1 = ["item1","item2"];
+// let array2 = array1;
+// console.log("array1", array1);
+// console.log("array2", array2);
+// array1.push("item3");
+// console.log("array1",array1);
+// console.log("array2",array2);
+// array 1 and array 2 will store in stack with a pointer of same address from the heap so any change in the array1 will also occure in array2
