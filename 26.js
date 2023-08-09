14b0366d8188c1c7efde0abe4355eba5b02216cb
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 20:16:28 +0500

    Arrays
    Intro to array , how to create arrays

---
+++ 26.js
@@ -0,0 +1,27 @@
+// intro to arrays 
+// arrays are reference data types
+
+// how to create arrays
+// ordered collection of items means every item will have an index number which will start from zero.
+// let fruits= ['apple','mango','grapes']; // strings array// we will use square brackets to make an array
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// let numbers = [1,2,3,4,5,6,7]; // numbers array
+// let mixed = ['car','bike',2018,2023,2.3,null,undefined] // mixed array 
+// console.log(mixed);
+// let fruits= ['apple','mango','grapes']; //  array is an object all reference data types are objects 
+// let obj = {
+//     // name :'nader',
+//     age : 34,
+// } // object litrel
+// fruits[1]= 'banana';
+// console.log(fruits);
+// console.log(typeof fruits); // check the type of array
+// console.log(typeof obj); // check the type of object
+
+// console.log(Array.isArray(fruits)); //  Check whether it is array or not
+// console.log(Array.isArray(obj));
+// array indexing
+
