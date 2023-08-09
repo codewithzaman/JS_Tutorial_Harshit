08941c1fd7bb06f1b070fffc5f1c4d014e18c48d
Nader Zaman <naderzaman50@gmail.com>
2023-08-09 20:51:39 +0500

    Array Methods
    Push , Pop , Shift and Unshift

---
+++ 27.js
@@ -0,0 +1,26 @@
+// array push and pop // They are fast 
+
+// array shift and unshift // they are slow then push and pop
+
+let fruits= ['apple','mango','grapes'];
+console.log(fruits);
+// push , it will add an element in the last of an array
+// fruits.push("banana"); // this is muteable method it will change the array into new array but in permittive data types it is not possible 
+// console.log(fruits);
+//pop
+// it will remove an element at the end of an array
+// let poppedFruit = fruits.pop();
+// console.log(fruits);
+// console.log("Popped Fruit is ",poppedFruit);
+
+// unshift 
+// unshift is used to add an element on the start
+
+// fruits.unshift("banana");
+// fruits.unshift("guava");
+// console.log(fruits);
+
+// shift 
+// shift will remove the elements from the start
+// let removedFruits = fruits.shift();
+// console.log("Removed Fruits is ",removedFruits);
