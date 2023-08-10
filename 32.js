b5ad19f981c3ba53e2963a0d869fb65faf7c4394
Nader Zaman <naderzaman50@gmail.com>
2023-08-10 13:18:46 +0500

    while loop in array

---
+++ 32.js
@@ -0,0 +1,17 @@
+// while loop in array
+
+// const fruits = ["mango","apple","banana",'grapes'];
+// let i =0;
+// while(i<fruits.length){
+//     console.log(fruits[i].toUpperCase());
+//     i++;
+// }
+
+const fruits = ["mango","apple","banana",'grapes'];
+const fruits2 =[];
+let i =0;
+while(i<fruits.length){
+    fruits2.push(fruits[i].toUpperCase());
+    i++;
+}
+console.log(fruits2);
