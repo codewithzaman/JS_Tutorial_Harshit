0b3b43afe2e096341623e9834c8a75ddf1dee388
Nader Zaman <naderzaman50@gmail.com>
2023-08-12 21:53:16 +0500

    added 35 file

--- 35.js
+++ 35.js
@@ -1,1 +1,18 @@
 // array destructuring
+// const myArray = ["value1","value2","value3"];
+const myArray = ["value1","value2","value3","value4"];
+// let myNewArray = myArray.slice(2);
+
+// let myVar1 = myArray[0];
+// let myVar2 = myArray[1];
+// console.log("Value of myVar1",myVar1);
+// console.log("Value of myVar2",myVar2);
+
+// let [myVar1,myVar2,myVar3] = myArray;
+let [myVar1,,myVar2,...myNewArray] = myArray;
+
+
+console.log("Value of myVar1",myVar1);
+console.log("Value of myVar2",myVar2);
+// console.log("Value of myVar3",myVar3);
+console.log("Value of myNewArray is", myNewArray);
