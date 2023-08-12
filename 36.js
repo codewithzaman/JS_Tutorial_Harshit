7f2757644aee90ccb279cf413f8ff90a3f87578d
Nader Zaman <naderzaman50@gmail.com>
2023-08-12 21:56:56 +0500

    Objects

---
+++ 36.js
@@ -0,0 +1,8 @@
+// Objects
+// arrays are good but not sufficient
+// for real world data
+// objects store key value pairs
+// objects don't have index
+// how to create objects
+// how to access data from objects
+// how to add key value pair to objects
