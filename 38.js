5662e2f389e9d89bdc3f81905c11de6eb50d0928
Nader Zaman <naderzaman50@gmail.com>
2023-08-13 14:26:52 +0500

    how to iterate an object

---
+++ 38.js
@@ -0,0 +1,10 @@
+// how to iterate an object
+
+const person = {
+    name : "nader",
+    age :34,
+    "person hobbies":["reading","cooking","sleeping"],
+}
+// we use to method for loop in objects
+// for in loop
+// object.keys
