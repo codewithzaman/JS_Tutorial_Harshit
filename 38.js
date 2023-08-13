4a19a00f16cc3c08995cfb21afbfdb1138ccc556
Nader Zaman <naderzaman50@gmail.com>
2023-08-13 14:37:08 +0500

    how to iterate an object

--- 38.js
+++ 38.js
@@ -1,5 +1,15 @@
 // how to iterate an object
 
+// const person = {
+//     name : "nader",
+//     age :34,
+//     "person hobbies":["reading","cooking","sleeping"],
+// }
+// // we use to method for loop in objects
+// // for in loop
+// for(let key in person){
+//     console.log(key);
+// }
 const person = {
     name : "nader",
     age :34,
@@ -7,4 +17,10 @@ const person = {
 }
 // we use to method for loop in objects
 // for in loop
+for(let key in person){
+    // console.log(person.key); // this will give undefined because it will not find a key with proerty named key 
+    // console.log(person[key]); // this will print value only
+    console.log(`${key} : ${person[key]}`); // this will print value and key pair both
+}
+
 // object.keys
