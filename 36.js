7fb1eeee404f2983449e7261ba9c827e109c25ce
Nader Zaman <naderzaman50@gmail.com>
2023-08-13 13:44:37 +0500

    how to create objects

--- 36.js
+++ 36.js
@@ -1,8 +1,11 @@
-// Objects
+// Objects is reference data type
 // arrays are good but not sufficient
 // for real world data
 // objects store key value pairs
 // objects don't have index
 // how to create objects
+const person = {name : "nader",age :34,} // object litrels
+console.log(person);
+console.log(typeof person);
 // how to access data from objects
 // how to add key value pair to objects
