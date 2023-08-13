68c16db6f81c936d873653c512731b0c02be0651
Nader Zaman <naderzaman50@gmail.com>
2023-08-13 13:54:17 +0500

    how to access data from objects

--- 36.js
+++ 36.js
@@ -4,8 +4,16 @@
 // objects store key value pairs
 // objects don't have index
 // how to create objects
-const person = {name : "nader",age :34,} // object litrels
+// const person = {name : "nader",age :34,} // object litrels // simple object
+const person = {
+    name : "nader",
+    age :34,
+    hobbies:["reading","cooking","sleeping"]
+}
 console.log(person);
-console.log(typeof person);
 // how to access data from objects
+// console.log(typeof person);
+// console.log(person.name);
+// console.log(person.age);
+console.log(person.hobbies);
 // how to add key value pair to objects
