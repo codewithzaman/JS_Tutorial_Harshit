662f78bb8f24373439c746199d85f361102c8f6c
Nader Zaman <naderzaman50@gmail.com>
2023-08-13 14:46:51 +0500

    iterating an object using object.keys and for of loop

--- 38.js
+++ 38.js
@@ -17,12 +17,20 @@ const person = {
 }
 // we use to method for loop in objects
 // for in loop
-for(let key in person){
-    // console.log(person.key); // this will give undefined because it will not find a key with proerty named key 
-    // console.log(person[key]); // this will print value only
-    // console.log(`${key} : ${person[key]}`); // this will print value and key pair both
-    console.log(key,":",person[key]);
+// for(let key in person){
+//     // console.log(person.key); // this will give undefined because it will not find a key with proerty named key 
+//     // console.log(person[key]); // this will print value only
+//     // console.log(`${key} : ${person[key]}`); // this will print value and key pair both
+//     console.log(key,":",person[key]);
 
-}
+// }
 
 // object.keys
+// console.log(Object.keys(person));
+// console.log(typeof Object.keys(person));
+// const val = Array.isArray(Object.keys(person));
+// console.log(val);
+
+for(let key of Object.keys(person)){
+    console.log(key,":",person[key]);
+}
