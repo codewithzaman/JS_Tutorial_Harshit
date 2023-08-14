d1cc597814f7e377ac1e48b2cf5c51cd505bb7ec
Nader Zaman <naderzaman50@gmail.com>
2023-08-14 13:18:39 +0500

    Update 43.js

--- 43.js
+++ 43.js
@@ -1,1 +1,16 @@
 // Nested De Structuring
+const users = [
+    {userid:1,firstName:"nader",gender:"male"},
+    {userid:2,firstName:"zaman",gender:"male"},
+    {userid:3,firstName:"zahida",gender:"female"},
+    
+]
+// const [user1,user2,user3] = users; // this will give full objects
+// console.log(user1);
+const [{firstName:pehlaName,userid},,{gender}]= users;
+// console.log(firstName);
+console.log(pehlaName);
+// console.log(gender);
+// console.log(user2);
+// console.log(user3);
+console.log(userid);
