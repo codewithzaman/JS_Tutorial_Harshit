5bd6e1c0e305580b605d009089008559c1e99521
Nader Zaman <naderzaman50@gmail.com>
2023-08-14 12:51:12 +0500

    updated objects de structuring

--- 41.js
+++ 41.js
@@ -1,1 +1,21 @@
 // objects destructuring
+const band = {
+    bandName : "led zepline",
+    famousSong : "Stairway to heaven",
+    year : 1970,
+    author : "Anthoney"
+};
+// const bandName = band.bandName;
+// const famousSong = band.famousSong;
+// console.log(bandName,famousSong);
+// console.log(bandName);
+// console.log(famousSong);
+// console.log(band); 
+// const {bandName,famousSong} = band; 
+// let {bandName,famousSong} = band; 
+let {bandName:var1,famousSong:var2} = band;
+let {bandName,famousSong,...restProps} = band; 
+famousSong = "Love me like you do"
+console.log(famousSong);
+console.log(var1,var2);
+console.log(restProps);
