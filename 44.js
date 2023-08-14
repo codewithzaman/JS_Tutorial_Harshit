c52eefa88a33fdacef70ae75fd74a849c5905582
Nader Zaman <naderzaman50@gmail.com>
2023-08-14 14:46:12 +0500

    Update 44.js

--- 44.js
+++ 44.js
@@ -26,8 +26,47 @@
 // console.log(returnedValue); 
 // This is not reusable function 
 // Function with Parameters will be reusable
-function SumTwoNumbers(number1,number2){
-        return number1+number2;
-    }
-    const returnedValue = SumTwoNumbers(45,56);
-    console.log(returnedValue);
+// function SumTwoNumbers(number1,number2){
+//         return number1+number2;
+//     }
+//     // const returnedValue = SumTwoNumbers(45,56);
+//     // const returnedValue = SumTwoNumbers();// If we will not a value of parameters NaN will print on console
+//     const returnedValue = SumTwoNumbers(undefined,undefined);
+//     console.log(returnedValue);
+    // function SumTwoNumbers(number1,number2,number3){
+    //     return number1+number2+number3;
+    // }
+    // const returnedValue = SumTwoNumbers(45,56,46);
+    // console.log(returnedValue);
+
+    // isEven
+    // input : 1 number
+    // output : true ,false
+
+    // function isEven(number){
+    //         if(number%2===0){
+    //             return true;
+    //         }else{
+    //             return false;
+    //         }
+    // }
+    // console.log(isEven(5));
+    // console.log(isEven(46));
+//     function isEven(number){
+//         if(number%2===0){
+//             return true;
+//         }
+//         return false;
+// }
+// console.log(isEven(5));
+// function isEven(number){
+//    return  number % 2 === 0;
+// }
+// console.log(isEven(14));\
+// function
+// input : string
+// output : firstcharacter
+function firstchar(anystring){
+    return anystring[0];
+}
+console.log(firstchar("xerox"));
