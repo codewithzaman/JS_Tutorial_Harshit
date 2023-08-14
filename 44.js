bfdc789c52ae0e417d8ef56070878374fda632ff
Nader Zaman <naderzaman50@gmail.com>
2023-08-14 13:28:40 +0500

    Functions

---
+++ 44.js
@@ -0,0 +1,19 @@
+// Function is a reusable code to do a task multiple times
+
+function birthDaySong(){
+    console.log("Happy BirthDay To You");
+}
+function towPlusFour(){
+    console.log(2+4);
+}
+// dry dont repeat urself
+birthDaySong();
+birthDaySong();
+birthDaySong();
+birthDaySong();
+birthDaySong();
+towPlusFour();
+towPlusFour();
+towPlusFour();
+towPlusFour();
+towPlusFour();
