c4ca49b4b702b495cf6e63d2bc1e1e43c2fde563
Nader Zaman <naderzaman50@gmail.com>
2023-08-14 13:40:04 +0500

    Function with parameters

--- 44.js
+++ 44.js
@@ -1,19 +1,33 @@
 // Function is a reusable code to do a task multiple times
 
-function birthDaySong(){
-    console.log("Happy BirthDay To You");
-}
-function towPlusFour(){
-    console.log(2+4);
-}
+// function birthDaySong(){
+//     console.log("Happy BirthDay To You");
+// }
+// function towPlusFour(){
+//     console.log(2+4);
+// }
 // dry dont repeat urself
-birthDaySong();
-birthDaySong();
-birthDaySong();
-birthDaySong();
-birthDaySong();
-towPlusFour();
-towPlusFour();
-towPlusFour();
-towPlusFour();
-towPlusFour();
+// birthDaySong();
+// birthDaySong();
+// birthDaySong();
+// birthDaySong();
+// birthDaySong();
+// towPlusFour();
+// towPlusFour();
+// towPlusFour();
+// towPlusFour();
+// towPlusFour();
+// function towPlusFour(){
+//     return 2+4;
+// }
+// // towPlusFour(); // this will not print any value
+// // console.log(towPlusFour());
+// const returnedValue = towPlusFour();
+// console.log(returnedValue); 
+// This is not reusable function 
+// Function with Parameters will be reusable
+function SumTwoNumbers(number1,number2){
+        return number1+number2;
+    }
+    const returnedValue = SumTwoNumbers(45,56);
+    console.log(returnedValue);
