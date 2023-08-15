275e82c09ee31038c1c21bb8d8e703e1d7fa456f
Nader Zaman <naderzaman50@gmail.com>
2023-08-15 18:14:53 +0500

    updated let and const

--- 50.js
+++ 50.js
@@ -2,3 +2,57 @@
 // let and const are function scope
 // var is block scope
 
+// {
+// let firstName = "Nader"; // { } this is a block
+// const lastName = "Zaman"; // we can only access let and const inside the block if we want to access let and const outside the block it will create an error.
+// console.log(firstName);
+// console.log(lastName);
+// }
+// // console.log(firstName);
+// // console.log(lastName);
+
+// {
+//     let firstName = "Zahid";
+//     const lastName = "Khan"
+//     console.log(firstName);
+//     console.log(lastName);
+// }
+// let firstName = "shahid";
+// console.log(firstName);
+
+// {
+//     var firstName = "Nader"; // we can access firstName outside the block when using Var as var has function or globle scope
+// }
+// console.log(firstName);
+// {
+// var firstName = "Zaman";
+// console.log(firstName);
+
+// }
+
+// {
+//     var firstName = "Khan";
+//     console.log(firstName);
+// }
+
+    // {
+    // var firstName = "Zaman";
+    // console.log(firstName);
+    
+    // }
+    
+    // {
+    //     console.log(firstName);
+    // }
+
+    // if(true){
+    //     let firstName = "Nader";
+    //     console.log(firstName);
+    // }
+    // console.log(firstName); //this will give error as let is used which is block scoped
+    
+    if(true){
+      var firstName = "Nader"; // this will pring firstname on console as Var is used which is goble scoped
+        console.log(firstName);
+    }
+    console.log(firstName);
