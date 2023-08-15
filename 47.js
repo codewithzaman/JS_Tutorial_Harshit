0857875b2831a97323e2c54550775f2f38af1f4b
Nader Zaman <naderzaman50@gmail.com>
2023-08-15 15:26:05 +0500

    hoisting

---
+++ 47.js
@@ -0,0 +1,19 @@
+// Hoisting
+
+// function hello(){
+//     console.log("Hello World");
+// }
+// hello();
+
+// hello();
+// function hello(){ // this will not give an error in function decleartion
+//     console.log("Hello World");
+// }
+// hello();
+// const hello = function (){ // this will give an error due to function expression
+//     console.log("Hello World");
+// }
+hello();
+const hello = ()=>{ // this will give an error due to function expression
+    console.log("Hello World");
+}
