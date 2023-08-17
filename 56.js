e877052a4bc0c2c4391033710872c2a0353daa36
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 21:39:35 +0500

    update forEach method

--- 56.js
+++ 56.js
@@ -18,9 +18,27 @@ const numbers = [4,5,3,6];
 // numbers.forEach(function(number,index){
 //     console.log(`index is ${index} and number is ${number}`);
 // })
-numbers.forEach(function(number){
-    console.log(number*2);
+// numbers.forEach(function(number){
+//     console.log(number*2);
+// })
+
+const users = [
+    {FirstName:"Nader",age:34,gender:"male"},
+    {FirstName:"Nafir",age:24,gender:"male"},
+    {FirstName:"Nasir",age:34,gender:"male"},
+    {FirstName:"Nasim",age:44,gender:"male"},
+    {FirstName:"Nater",age:54,gender:"male"},
+
+]
+// users.forEach(function(user){
+//     console.log(user.FirstName);
+// })
+users.forEach((user)=>{
+    console.log(user.FirstName);
 })
+// for(let user of users){
+//     console.log(user.FirstName);
+// }
 
 //Map 
 
