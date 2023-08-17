acff7b0f5451f7a27d8f0c5a45dba6b70beab0c9
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 21:10:40 +0500

    Update 56.js

--- 56.js
+++ 56.js
@@ -1,5 +1,18 @@
 // Important Array methods 
 
+const numbers = [4,5,3,6];
+
+function multiplyBy2(number,index){
+    console.log(`index is ${index} and number is ${number}`);
+    // console.log(`${number}*2 = ${number*2}`);
+}
+// multiplyBy2(numbers[0],0);
+// multiplyBy2(numbers[1],1);
+
+for(let i =0 ; i < numbers.length ; i++){
+    multiplyBy2(numbers[i],i);
+}
+
 // ForEach 
 
 //Map 
