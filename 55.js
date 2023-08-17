803c25ac3ac7d576a3d4678b9355e0d69f0e09ab
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 17:02:25 +0500

    Function returning function

---
+++ 55.js
@@ -0,0 +1,39 @@
+// Function returning function
+
+// function myFunc(){
+//     return "a";
+
+// }
+// const ans = myFunc();
+// console.log(ans);
+
+// function myFunc(){
+//     return [1,2,3];
+
+// }
+// const ans = myFunc();
+// console.log(ans);
+
+// function myFunc(){
+//     return {name:"nader",age:34};
+
+// }
+// const ans = myFunc();
+// console.log(ans);
+
+// function myFunc(){
+//     return 344;
+
+// }
+// const ans = myFunc();
+// console.log(ans);
+
+function myFunc(){
+    function hello(){
+        
+    }
+    return 344;
+
+}
+const ans = myFunc();
+console.log(ans);
