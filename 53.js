06e4229be955c7f498fbb9ba7926ba1bf9815ebc
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 15:51:11 +0500

    parameter De structuring

---
+++ 53.js
@@ -0,0 +1,13 @@
+// parameter Destructuring 
+
+// objects
+// react
+
+const person = {
+    firstName : "Nader",
+    gender : "male"
+}
+function printDetails(obj){
+    console.log(obj.firstName);
+    console.log(obj.gender);
+}
