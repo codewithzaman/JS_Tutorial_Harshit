a3c0eaa08e056b80a38a9310a8e8227840089c57
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 15:57:35 +0500

    Update 53.js

--- 53.js
+++ 53.js
@@ -5,9 +5,18 @@
 
 const person = {
     firstName : "Nader",
-    gender : "male"
+    gender : "male",
+    age : 34,
 }
-function printDetails(obj){
-    console.log(obj.firstName);
-    console.log(obj.gender);
+// function printDetails(obj){
+//     console.log(obj.firstName);
+//     console.log(obj.gender);
+//     console.log(obj.age);
+// }
+function printDetails({firstName,gender,age}){
+    console.log(firstName);
+    console.log(gender);
+    console.log(age);
 }
+
+printDetails(person);
