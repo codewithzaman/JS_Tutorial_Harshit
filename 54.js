f17fcbd15aa763f914536aaeda2f2a3bd851f658
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 16:14:56 +0500

    Call Back Functions

---
+++ 54.js
@@ -0,0 +1,18 @@
+// CallBack Functions
+
+// 
+function myFunc(callBack){
+    console.log("I am inside myFunc");
+    callBack("nader");
+}
+// function myFunc2(){
+//     console.log("Inside MyFunc2");
+// }
+function myFunc2(name){
+    console.log("Inside MyFunc2");
+    console.log(`my name is ${name} `);
+}
+// myFunc([1,2,3,4]);
+// myFunc("abcdef");
+// myFunc({Name:"Nader"});
+myFunc(myFunc2);
