3af022ce852010d17a61324b1d5eb9d4a92e4b45
Nader Zaman <naderzaman50@gmail.com>
2023-08-17 21:26:27 +0500

    ForEach Loop

--- 56.js
+++ 56.js
@@ -2,18 +2,25 @@
 
 const numbers = [4,5,3,6];
 
-function multiplyBy2(number,index){
-    console.log(`index is ${index} and number is ${number}`);
+// function multiplyBy2(number,index){
+//     console.log(`index is ${index} and number is ${number}`);
     // console.log(`${number}*2 = ${number*2}`);
-}
+// }
 // multiplyBy2(numbers[0],0);
 // multiplyBy2(numbers[1],1);
 
-for(let i =0 ; i < numbers.length ; i++){
-    multiplyBy2(numbers[i],i);
-}
+// for(let i =0 ; i < numbers.length ; i++){
+//     multiplyBy2(numbers[i],i);
+// }
 
-// ForEach 
+// ForEach
+// numbers.forEach(multiplyBy2);
+// numbers.forEach(function(number,index){
+//     console.log(`index is ${index} and number is ${number}`);
+// })
+numbers.forEach(function(number){
+    console.log(number*2);
+})
 
 //Map 
 
